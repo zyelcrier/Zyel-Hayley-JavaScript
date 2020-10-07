b6e2396300ea66ae1882bf7b866fc5012a8aff0b
zyelcrier <65629240+zyelcrier@users.noreply.github.com>
2020-10-06 22:11:47 -0500

    fix csv names and set website frame

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 function test() {
 	document.getElementById("test").innerHTML = "its working if you can see this";
 }
+
+//Slideshow Function
+var i = 0;
+var images = [
+  "/bro.jpg"
+];
+var time = 3000;
+
+function changeImg() {
+  document.slide.src = images[i];
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
+
